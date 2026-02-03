a716dffa3ef01388fbc37aa3f1033464a8bdddb1
root <root@172.16.117.251>
2026-02-03 13:58:45 +0000

    chore: reset project to clean state (automated)

--- frontend/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { LogoInputForm } from './LogoInputForm';
-export { LoadingState } from './LoadingState';
-export { LogoPreview } from './LogoPreview';
-export { ProgressLoader } from './ProgressLoader';
-export { LogoHistory } from './LogoHistory';
-export { LogoVariations } from './LogoVariations';
-export type { LogoHistoryItem } from './LogoHistory';
